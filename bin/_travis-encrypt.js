8b2c3a6b148fb8fc9b8fffa5d80d3dfc44ef701c
bluelovers <codelovers@users.sourceforge.net>
2018-05-17 01:45:59 +0800

    .

--- bin/_travis-encrypt.js
+++ bin/_travis-encrypt.js
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
+console.log(process.env.SOMEVAR == 'test');
 console.log('SOMEVAR=', process.env.SOMEVAR);
--- bin/_travis-encrypt.ts
+++ bin/_travis-encrypt.ts
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
+console.log(process.env.SOMEVAR == 'test');
+
 console.log('SOMEVAR=', process.env.SOMEVAR);
