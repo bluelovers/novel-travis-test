152c361249d448ae6815a2c9989c6888bc9cd98c
bluelovers <codelovers@users.sourceforge.net>
2018-07-28 15:36:41 +0800

    加入 git 紀錄

--- bin/_create_toc.js
+++ bin/_create_toc.js
@@ -3,9 +3,19 @@
  * Created by user on 2018/7/28/028.
  */
 Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../index");
 const project_config_1 = require("../project.config");
 // @ts-ignore
 const toc_1 = require("@node-novel/toc");
 toc_1.processToc(project_config_1.default.novel_root)
     .then(function (ls) {
+    return index_1.crossSpawnAsync('git', [
+        'commit',
+        '-a',
+        '-m',
+        `[TOC] auto update toc`,
+    ], {
+        stdio: 'inherit',
+        cwd: project_config_1.default.novel_root,
+    });
 });
--- bin/_create_toc.ts
+++ bin/_create_toc.ts
@@ -2,6 +2,7 @@
  * Created by user on 2018/7/28/028.
  */
 
+import { crossSpawnSync, crossSpawnAsync } from '../index';
 import ProjectConfig from '../project.config';
 // @ts-ignore
 import { processToc } from '@node-novel/toc';
@@ -9,6 +10,14 @@ import { processToc } from '@node-novel/toc';
 processToc(ProjectConfig.novel_root)
 	.then(function (ls)
 	{
-
+		return crossSpawnAsync('git', [
+			'commit',
+			'-a',
+			'-m',
+			`[TOC] auto update toc`,
+		], {
+			stdio: 'inherit',
+			cwd: ProjectConfig.novel_root,
+		});
 	})
 ;
