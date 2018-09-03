ae4ac46ff0fa85bd9b163a52236c9dc34b24d04a
bluelovers <codelovers@users.sourceforge.net>
2018-09-03 14:19:45 +0800

    .

--- lib/log.ts
+++ lib/log.ts
@@ -2,15 +2,21 @@
  * Created by user on 2018/9/3/003.
  */
 
+if (process && process.env)
+{
+	process.env.FORCE_COLOR = '1';
+}
+
 import { Console } from 'debug-color2';
 export const console = new Console();
 
-console.enabledColor = true;
-
 console.setOptions({
+	enabled: true,
 	inspectOptions: {
 		colors: true,
 	}
 });
+
+console.enabledColor = true;
 
 export default console
