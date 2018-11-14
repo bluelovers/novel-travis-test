a6b79722a63cbab9ed4dcaceb79ba42ddd269dfa
bluelovers <codelovers@users.sourceforge.net>
2018-11-14 19:59:42 +0800

    增加於 根目錄 的 toc

--- bin/_create_toc.ts
+++ bin/_create_toc.ts
@@ -2,6 +2,7 @@
  * Created by user on 2018/7/28/028.
  */
 
+import { createTocRoot } from '@node-novel/toc/toc-root';
 import { crossSpawnSync, crossSpawnAsync } from '../index';
 import ProjectConfig from '../project.config';
 // @ts-ignore
@@ -9,10 +10,10 @@ import { processToc, Promise } from '@node-novel/toc';
 import path = require('path');
 import console from '../lib/log';
 
-processToc(ProjectConfig.novel_root)
+let p = processToc(ProjectConfig.novel_root)
 	.then(async function (ls)
 	{
-		await Promise.each(Object.keys(ls), function(pathMain)
+		await Promise.each(Object.keys(ls), function (pathMain)
 		{
 			let file = path.join(ProjectConfig.novel_root, pathMain, 'README.md');
 
@@ -35,5 +36,36 @@ processToc(ProjectConfig.novel_root)
 			stdio: 'inherit',
 			cwd: ProjectConfig.novel_root,
 		});
+	})
+;
+
+Promise.resolve(p)
+	.finally(function ()
+	{
+		return createTocRoot(ProjectConfig.novel_root, path.join(ProjectConfig.novel_root, 'README.md'))
+			.tap(async function ()
+			{
+				let file = path.join(ProjectConfig.novel_root, 'README.md');
+
+				await crossSpawnAsync('git', [
+					'add',
+					'--verbose',
+					file,
+				], {
+					stdio: 'inherit',
+					cwd: ProjectConfig.novel_root,
+				});
+
+				return crossSpawnAsync('git', [
+					'commit',
+					'-a',
+					'-m',
+					`[TOC] toc root`,
+				], {
+					stdio: 'inherit',
+					cwd: ProjectConfig.novel_root,
+				});
+			})
+		;
 	})
 ;
